76b8556a28368244d06a4a7014eba54b5310f758
Sourabhdalal <87751132+Sourabhdalal@users.noreply.github.com>
2024-02-19 14:19:43 +0530

    Projects Uploaded

---
+++ Doctor-At-Call/src/main/java/com/DoctorApi/blog/Dto/PrescriptionDto.java
@@ -0,0 +1,27 @@
+package com.DoctorApi.blog.Dto;
+
+import com.DoctorApi.blog.Entity.Doctor;
+import com.DoctorApi.blog.Entity.Patient;
+
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PrescriptionDto 
+{
+	private Integer Id;
+	
+	private String dieases;
+	private String tablet;
+	private String date;
+	private String time;
+	
+	private Doctor doctor;
+
+	private Patient patient;
+}
